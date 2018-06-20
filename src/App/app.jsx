104ca28728bdeaf24d5689c21207272769867ed7
Jonathan <jkrone@vt.edu>
2018-06-20 15:24:22 -0500

    feat: style progress, checkpoint of working scrollbars

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import cn from 'classnames';
 
+import { shell } from 'electron';
+
 import { loadFromGSheets, labelRows } from './../GoogleSheets/load-from-gsheets';
 import { fillPDFs } from './../fill-form';
 import ErrorBoundary from './ErrorBoundary';
+import utilityToPdf from './utility-to-pdf';
+
+const { dialog } = require('electron').remote;
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { labels: [], rows: [], selectedRows: [] };
+    this.state = {
+      labels: [],
+      data: [],
+      rows: [],
+      selectedRows: [],
+      selectedFiles: [],
+      outputFolder: null,
+    };
     this.selectRow = this.selectRow.bind(this);
-    this.handleFileSelect = this.handleFileSelect.bind(this);
     this.fillEm = this.fillEm.bind(this);
+    this.showFormSelect = this.showFormSelect.bind(this);
+    this.showFolderSelect = this.showFolderSelect.bind(this);
+    this.filterRows = this.filterRows.bind(this);
   }
 
   componentWillMount() {
-    loadFromGSheets().then(({ labels, rows }) => this.setState({ labels, rows: rows.slice(0, 5) }));
+    loadFromGSheets().then(({ labels, rows }) =>
+      this.setState({ labels, data: rows.slice(0, 50), rows: rows.slice(0, 50) }, () => {
+        const s = new Set();
+        rows.forEach(row => s.add(row[3]));
+        console.log('utilities:', s);
+      }),
+    );
   }
 
   selectRow(idx) {
-    console.log('selecting idx:', idx);
     this.setState(({ selectedRows }) => ({
       selectedRows: selectedRows.includes(idx)
         ? selectedRows.filter(row => row !== idx)
@@ -28,27 +47,62 @@ export default class App extends React.Component {
   }
 
   fillEm() {
-    const files = this.state.selectedFiles;
-    const rows = this.state.selectedRows.map(idx => this.state.rows[idx]);
+    if (this.state.selectedRows.length <= 0) {
+      console.warn('Please select a row!');
+      return;
+    }
+    const pdfs = this.state.selectedFiles;
+    const rows = this.state.selectedRows.map(idx => this.state.data[idx]);
     const labeledData = labelRows(this.state.labels, rows);
-    console.log('labeled Data:', labeledData);
-    console.log('paths:', files);
 
     fillPDFs({
-      pdfPaths: files,
+      pdfPaths: pdfs,
       data: labeledData,
       nametag: 'Customer Name',
-      outputFolder: "C:/Projects/professional/FormFunnel/src/App",
-    })
+      outputFolder: this.state.outputFolder,
+    }).then((filled) => {
+      shell.showItemInFolder(filled[0]);
+      console.log('filled:', filled);
+    });
   }
 
-  handleFileSelect(e) {
-    console.log('e?:', e.target.files);
-    // Note: input.files are type FileList and not directly iterable
-    const files = e.target.files;
+  showFormSelect() {
+    dialog.showOpenDialog(
+      {
+        title: 'Select fillable PDFs',
+        filters: [
+          { name: 'PDFs', extensions: ['pdf', 'fdf'] },
+          { name: 'All Files', extensions: ['*'] },
+        ],
+        properties: ['openFile', 'multiSelections'],
+      },
+      (selectedFiles) => {
+        console.log('selected files:', selectedFiles);
+        this.setState({ selectedFiles });
+      },
+    );
+  }
 
+  showFolderSelect() {
+    // maybe just use a default dir
+    dialog.showOpenDialog(
+      { title: 'Where to save created PDF', properties: ['openDirectory'] },
+      (folder) => {
+        console.log('folder:', folder[0]);
+        this.setState({ outputFolder: folder[0] });
+      },
+    );
+  }
+
+  filterRows(e) {
+    const value = e.target.value;
+    const data = this.state.data;
+    const selectedRows = this.state.selectedRows;
+    const rows = data.filter(
+      (row, idx) => selectedRows.includes(idx) || !!row.find(col => col.includes(value)),
+    );
     this.setState({
-      selectedFiles: Object.keys(files).map(fileIdx => files[fileIdx].path),
+      rows,
     });
   }
 
@@ -56,59 +110,84 @@ export default class App extends React.Component {
     const { labels, rows, selectedRows } = this.state;
     return (
       <ErrorBoundary>
-        <h2>Welcome to React!</h2>
         {labels ? (
           <React.Fragment>
-            <div className="fl w-50">
-              <button
-                className="f4 fr link grow br3 ph3 pv2 mb2 dib white bg-dark-blue"
-                onClick={this.fillEm}
-              >
-                Fill 'em!
-              </button>
-              <table className="sheets-table collapse ba b--black-10">
-                <thead>
-                  <tr className="striped--near-white bb">
-                    {labels.map((label, idx) => (
-                      <th className="pv2 ph2 tl f6 fw6 ttu" key={`${label}+${idx}`}>
-                        {label}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {rows.map((row, idx) => (
-                    <tr
-                      className={cn(
-                        { 'sheets-table__row--selected': selectedRows.includes(idx) },
-                        'sheets-table__row striped--near-white mv1',
-                      )}
-                      onClick={() => this.selectRow(idx)}
-                      key={`${row[0]}${idx}`}
-                    >
-                      {row.map((col, idx) => (
-                        <td className="ph2 pv1" key={`${col}${idx}`}>
-                          {col}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-            <div className="fl w-50">
+            <div className="dib flex flex-column items-baseline">
               <input
-                type="file"
-                ref="files"
-                multiple
-                accept=".pdf, .fdf"
-                onChange={(...args) => this.handleFileSelect(...args)}
-                className="bg-dark-blue"
+                className="mv3 mh4 pv2 ph3 ba br2"
+                type="text"
+                placeholder="Filter"
+                onChange={this.filterRows}
               />
+              <div className="w-50 vh-80 ph3 br2 overflow-auto">
+                <div className="overflow-auto">
+                  <table className="sheets-table mh3 collapse ba b--black-10">
+                    <TableHeader labels={labels} />
+                    <TableRows rows={rows} selectedRows={selectedRows} selectRow={this.selectRow} />
+                  </table>
+                </div>
+              </div>
+              <div className="w-50 vh-80 action-panel">
+                <button
+                  className={'f4 fr link br3 ph3 pv2 mb2 dib white bg-dark-blue'}
+                  onClick={this.showFormSelect}
+                >
+                  Select Forms
+                </button>
+                <button
+                  className={'f4 fr link br3 ph3 pv2 mb2 dib white bg-dark-blue'}
+                  onClick={this.showFolderSelect}
+                >
+                  Output Folder
+                </button>
+              </div>
             </div>
+            <button
+              className="f4 fr link br3 ph3 pv2 mb2 dib white bg-dark-blue"
+              onClick={this.fillEm}
+            >
+              Fill 'em!
+            </button>
           </React.Fragment>
         ) : null}
       </ErrorBoundary>
     );
   }
+}
+
+function TableHeader({ labels }) {
+  return (
+    <thead>
+      <tr className="striped--near-white bb">
+        {labels.map((label, idx) => (
+          <th className="pv2 ph2 tl f6 fw6 ttu" key={`${label}+${idx}`}>
+            {label}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
+
+function TableRows({ rows, selectedRows, selectRow }) {
+  return (
+    <tbody>
+      {rows.map((row, idx) => (
+        <tr
+          className={cn(
+            { 'sheets-table__row--selected': selectedRows.includes(idx) },
+            'sheets-table__row striped--near-white mv1',
+          )}
+          onClick={() => selectRow(idx)}
+          key={`${row[0]}${idx}`}
+        >
+          {row.map((col, idx) => (
+            <td className="ph2 pv1" key={`${col}${idx}`}>
+              {col}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
 }
---
+++ src/App/utility-to-pdf.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AE: ['C:\\Projects\\professional\\FormFunnel\\test\\fixtures\\AE CAF 2018.pdf'],
+  AEP: [
+    'C:\\Projects\\professional\\FormFunnel\\test\\fixtures\\AEP Texas Interconnection Application.pdf',
+  ],
+};
