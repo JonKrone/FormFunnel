ad5063b42a2c95a8021be97fda40b648b3eaca9f
jonkrone <jonathankrone@gmail.com>
2018-07-12 15:41:13 -0500

    feat: initial work on handling Squirrel events

---
+++ src/App/core/squirrels.js
@@ -0,0 +1,70 @@
+import { app } from 'electron'
+import ChildProcess from 'child_process'
+import path from 'path'
+
+const INSTALL = '--squirrel-install'
+const UPDATE = '--squirrel-updated'
+const UNINSTALL = '--squirrel-uninstall'
+const OBSOLETE = '--squirrel-obsolete'
+
+export default function handleSquirrelEvent() {
+  const isSquirrel = [INSTALL, UPDATE, UNINSTALL, OBSOLETE].includes(
+    process.argv[1]
+  )
+  if (!isSquirrel) {
+    // naive squirrel check, may conflict args in the future
+    return false
+  }
+
+  const appFolder = path.resolve(process.execPath, '..')
+  const rootAtomFolder = path.resolve(appFolder, '..')
+  const squirrel = path.resolve(path.join(rootAtomFolder, 'Update.exe'))
+  const exeName = path.basename(process.execPath)
+
+  const squirrelEvent = process.argv[1]
+  switch (squirrelEvent) {
+    case INSTALL:
+    case UPDATE:
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawn(squirrel, ['--createShortcut', exeName])
+
+      setTimeout(app.quit, 1000)
+      return true
+    case UNINSTALL:
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawn(['--removeShortcut', exeName])
+
+      setTimeout(app.quit, 1000)
+      return true
+
+    case OBSOLETE:
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit()
+      return true
+  }
+
+  return false
+}
+
+function spawn(command, args) {
+  let spawnedProcess
+  try {
+    spawnedProcess = ChildProcess.spawn(command, args, { detached: true })
+  } catch (error) {
+    console.error('Failed to handle a squirrel event:', command, args)
+    console.error(error)
+  }
+
+  return spawnedProcess
+}
