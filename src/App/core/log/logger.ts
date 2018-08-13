2b456645599940e8aba41a80debf27e0169bfd5d
Jonathan Krone <JonathanKrone@gmail.com>
2018-08-13 14:13:31 -0500

    feat: tsc logger.js

--- src/App/core/log/logger.ts
+++ src/App/core/log/logger.ts
@@ -1,8 +1,28 @@
 import uuid from 'uuid/v4'
 import Datastore from '@google-cloud/datastore'
 
-const defaultLogs = []
-let logger
+import ElectronStore from 'electron-store'
+
+enum LogType {
+  Error = 'error',
+  SaveSession = 'save-session',
+}
+
+interface Log {
+  created: number
+  sessionId: string
+  type: LogType
+  error: any
+}
+
+interface Logger {
+  (payload: Partial<Log>): void
+  error: (payload: Partial<Log>) => void
+  flush: () => void
+}
+
+const defaultLogs: Log[] = []
+let logger: Logger
 
 const isDev = process.env.NODE_ENV !== 'production'
 
@@ -15,13 +35,13 @@ const datastore = new Datastore({
 })
 
 // TODO: store and submit logs grouped by 'session-id'
-export default function createLoger(store) {
+export default function createLoger(store: ElectronStore): Logger {
   const sessionId = uuid()
   const logKey = 'logs'
 
   initSession(store, sessionId)
 
-  function log(payload) {
+  function log(payload: any): void {
     if (typeof payload !== 'object') {
       throw new Error('You may only log Objects')
     }
@@ -37,47 +57,48 @@ export default function createLoger(store) {
     store.set(logKey, oldLogs.concat([payload]))
   }
 
-  log.error = payload => log(Object.assign(payload, { type: 'error' }))
+  logger = Object.assign(log, {
+    error: (payload: Partial<Log>) =>
+      log(Object.assign(payload, { type: LogType.Error })),
+    flush: function flushLogs(): Promise<void> {
+      const now = Date.now()
+      const localLogs = <Log[]>store.get('logs')
+      if (!localLogs || !localLogs.length) {
+        console.warn('Attempting to flush logs when there are none.')
+        return Promise.resolve()
+      }
 
-  log.flush = function flushLogs() {
-    const now = Date.now()
-    const localLogs = store.get('logs')
-    if (!localLogs || !localLogs.length) {
-      console.warn('Attempting to flush logs when there are none.')
-      return
-    }
+      const logs = {
+        key: datastore.key(['Session', sessionId]),
+        data: {
+          logs: localLogs,
+        },
+      }
 
-    const logs = {
-      key: datastore.key(['Session', sessionId]),
-      data: {
-        logs: localLogs,
-      },
-    }
+      return datastore
+        .save(logs)
+        .then(() => {
+          const nextLogs = <Log[]>store.get('logs')
+          store.set('logs', nextLogs.filter(nextLog => nextLog.created > now))
+          log('Uploaded previous logs')
+        })
+        .catch((error: Error) => {
+          console.error('Oh man, looks like GCP had an error uploading logs')
+          throw error
+        })
+    },
+  })
 
-    datastore
-      .save(logs)
-      .then(() => {
-        const nextLogs = store.get('logs')
-        store.set('logs', nextLogs.filter(nextLog => nextLog.created > now))
-        log('Uploaded previous logs')
-      })
-      .catch(error => {
-        console.error('Oh man, looks like GCP had an error uploading logs')
-        throw error
-      })
-  }
-
-  logger = log
-  return log
+  return logger
 }
 
-function initSession(store, sessionId) {
+function initSession(store: ElectronStore, sessionId: string) {
   // Hard-coded users for now. Leaving room to make it dynamic later.
   const isProd = process.env.NODE_ENV === 'production'
   const user = isProd ? 'david' : 'dev'
   const userKey = datastore.key(['User', user])
 
-  return datastore.get(userKey).then(([entity]) => {
+  return datastore.get(userKey).then(([entity]: any[]) => {
     const lastSessionId = entity.sessions[entity.sessions.length - 1]
     return Promise.all([
       !!lastSessionId && storeSession(store, lastSessionId),
@@ -85,12 +106,12 @@ function initSession(store, sessionId) {
         Object.assign(entity, {
           sessions: (entity.sessions || []).concat([sessionId]),
         })
-      )
+      ),
     ])
   })
 }
 
-function storeSession(store, sessionId) {
+function storeSession(store: ElectronStore, sessionId: string) {
   const logs = store.get('logs')
   if (logs.length <= 0) return Promise.resolve()
   store.set('logs', defaultLogs)
@@ -105,14 +126,14 @@ function storeSession(store, sessionId) {
 
   return datastore
     .get(sessionKey)
-    .then(([sesh]) => {
+    .then(([sesh]: any[]) => {
       if (sesh) {
         // updating a session
         session.data.logs = sesh.logs.concat(logs)
       }
       return datastore.upsert(session)
     })
-    .catch(error => {
+    .catch((error: Error) => {
       console.warn(
         "Failed to submit last session's logs. Resetting logs to:",
         logs
@@ -120,7 +141,7 @@ function storeSession(store, sessionId) {
       store.set('logs', logs)
       if (logger) {
         logger({
-          type: 'save-session',
+          type: LogType.SaveSession,
           error,
         })
       }
