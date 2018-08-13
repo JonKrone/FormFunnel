beaa9021044907f9109dfdac47d5cb7c4b894938
Jonathan Krone <JonathanKrone@gmail.com>
2018-08-13 11:19:40 -0500

    feat: tsc fill-form.js

--- src/App/core/pdf/fill-form.ts
+++ src/App/core/pdf/fill-form.ts
@@ -1,18 +1,30 @@
 import path from 'path'
 import fs from 'fs'
 import { promisify } from 'util'
-import pdf from 'pdffiller'
+import { fillFormWithFlatten } from 'pdffiller'
 import chalk from 'chalk'
 
 import parseCSV from './../loaders/parse-csv'
 
-const fill = promisify(pdf.fillFormWithFlatten).bind(pdf)
+const fill = promisify(fillFormWithFlatten)
 const mkdir = promisify(fs.mkdir)
 
 // How many of the last path.sep separated paths to include in the output path
 const RELATIVE_OUTPUT_PATH_PART = -3
 
-export async function fillPDFs({ pdfPaths, outputFolder, quiet, data }) {
+interface PdfOptions {
+  pdfPaths: string[] | string
+  outputFolder: string
+  quiet: boolean
+  data: any[]
+}
+
+export async function fillPDFs({
+  pdfPaths,
+  outputFolder,
+  quiet,
+  data,
+}: PdfOptions): Promise<string[]> {
   if (!Array.isArray(pdfPaths)) {
     pdfPaths = [pdfPaths]
   }
@@ -24,7 +36,7 @@ export async function fillPDFs({ pdfPaths, outputFolder, quiet, data }) {
     throw new Error(`Output folder ${outputFolder} does not exist! Typo?`)
   }
 
-  const filledForms = []
+  const filledForms: string[] = []
   const formFills = pdfPaths
     .map(pdfPath =>
       data.map(row => {
@@ -43,24 +55,26 @@ export async function fillPDFs({ pdfPaths, outputFolder, quiet, data }) {
         })
       })
     )
-    .reduce((a, i) => a.concat(i), [])
+    .reduce((a, i) => a.concat(i), []) // flatten
 
   return Promise.all(formFills).then(() => filledForms)
 }
 
-export async function fillFromCSV({ pdfPaths, csvPath, outputFolder, quiet }) {
-  const { labels, data } = await parseCSV(csvPath)
+export async function fillFromCSV(
+  { pdfPaths, outputFolder, quiet }: PdfOptions,
+  csvPath: string
+) {
+  const { data } = await parseCSV(csvPath)
   await fillPDFs({
     pdfPaths,
     outputFolder,
     quiet,
-    labels,
     data,
   })
 }
 
-export function fillPDF(pdfPath, outputPath, data) {
-  return fill(pdfPath, outputPath, data, false).catch(error => {
+export function fillPDF(pdfPath: string, outputPath: string, data: any[]) {
+  return fill(pdfPath, outputPath, data, false).catch((error: Error) => {
     if (error.message.match(/spawn pdftk ENOENT/)) {
       console.log('Looks like we dont have pdftk')
     }
@@ -69,8 +83,8 @@ export function fillPDF(pdfPath, outputPath, data) {
   })
 }
 
-async function ensureFolderExists(folderPath) {
-  return mkdir(folderPath).catch(error => {
+async function ensureFolderExists(folderPath: string) {
+  return mkdir(folderPath).catch((error: NodeJS.ErrnoException) => {
     if (error.code === 'EEXIST') return Promise.resolve()
     throw error
   })
